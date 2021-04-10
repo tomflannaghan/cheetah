611065506548820449da5b1af13de4aa35ce0884
Tom Flannaghan <tomflannaghan@gmail.com>
2021-04-10 17:04:14 +0100

    A little bit of styling

--- common/src/commonMain/kotlin/com/flannaghan/cheetah/common/App.kt
+++ common/src/commonMain/kotlin/com/flannaghan/cheetah/common/App.kt
@@ -1,18 +1,24 @@
 package com.flannaghan.cheetah.common
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun App() {
     var text by remember { mutableStateOf("Hello, World!") }
 
     MaterialTheme {
-        Button(onClick = {
-            text = "Hello, ${getPlatformName()}"
-        }) {
-            Text(text)
+        Column(modifier = Modifier.padding(16.dp)) {
+            Button(onClick = {
+                text = "Hello, ${getPlatformName()}"
+            }) {
+                Text(text)
+            }
         }
     }
 }
