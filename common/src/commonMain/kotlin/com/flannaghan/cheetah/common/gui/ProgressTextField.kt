e9da9dc90e583ae977d674bb0c8561b91f34cb81
Tom Flannaghan <tomflannaghan@gmail.com>
2021-04-21 12:09:21 +0100

    Improve alignment of progress indicator

--- common/src/commonMain/kotlin/com/flannaghan/cheetah/common/gui/ProgressTextField.kt
+++ common/src/commonMain/kotlin/com/flannaghan/cheetah/common/gui/ProgressTextField.kt
@@ -1,15 +1,15 @@
 package com.flannaghan.cheetah.common.gui
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.TextField
 import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun ProgressTextField(
@@ -34,11 +34,15 @@ fun ProgressTextField(
                 keyboardOptions = keyboardOptions
             )
         }
-        Row {
-            Spacer(modifier = Modifier.weight(1.0f))
+        Row(
+            horizontalArrangement = Arrangement.End,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.matchParentSize()
+        ) {
             if (isBusy) {
                 CircularProgressIndicator()
             }
+            Spacer(modifier = Modifier.width(10.dp))
         }
     }
 }
