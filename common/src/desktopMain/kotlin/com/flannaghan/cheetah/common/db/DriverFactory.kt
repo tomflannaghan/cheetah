12b918f0a6f25ec50e859d5106fe5156e7086a59
Tom Flannaghan <tomflannaghan@gmail.com>
2021-04-24 20:10:28 +0100

    Update and include the wiktionary database.

--- common/src/desktopMain/kotlin/com/flannaghan/cheetah/common/db/DriverFactory.kt
+++ common/src/desktopMain/kotlin/com/flannaghan/cheetah/common/db/DriverFactory.kt
@@ -5,13 +5,13 @@ import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
 
 actual class DatabaseDriverFactory {
     actual fun createDriver(): SqlDriver {
-        return JdbcSqliteDriver("jdbc:sqlite:/home/tom/src/cheetah/raw_data/processed.db")
+        return JdbcSqliteDriver("jdbc:sqlite:/home/tom/src/cheetah/data/wiktionary.sqlite")
     }
 
     fun doStuff() {
         val driver = DatabaseDriverFactory().createDriver()
         val db = WordDatabase(driver)
-        val words = db.wordDatabaseQueries.selectAllWords().executeAsList()
-        println(words.first())
+        val words = db.wordQueries.selectAllWithLimit(1).executeAsList()
+        println(words)
     }
 }
