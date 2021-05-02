84e4187a76e40cc892169b5b5f2b94aa3ddc8017
Tom Flannaghan <tomflannaghan@gmail.com>
2021-05-02 18:22:35 +0100

    Fix bug in desktop

--- desktop/src/jvmMain/kotlin/main.kt
+++ desktop/src/jvmMain/kotlin/main.kt
@@ -1,7 +1,6 @@
 import androidx.compose.desktop.Window
 import com.flannaghan.cheetah.common.DesktopApplicationContext
 import com.flannaghan.cheetah.common.DesktopSearchModel
-import com.flannaghan.cheetah.common.db.DatabaseDriverFactory
 import com.flannaghan.cheetah.common.gui.App
 import com.flannaghan.cheetah.common.wordSources
 
@@ -9,7 +8,6 @@ private val CONTEXT = DesktopApplicationContext()
 private val SEARCHER = DesktopSearchModel()
 
 fun main() = Window {
-    DatabaseDriverFactory().doStuff()
     SEARCHER.wordSources = wordSources(CONTEXT)
     App(SEARCHER)
 }
