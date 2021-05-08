36aedf265654caae9339504da5a928fbc01abb45
Tom Flannaghan <tomflannaghan@gmail.com>
2021-05-08 11:39:12 +0100

    Instead of scrolling to make visible always, only do so if either the items change or the invisibility was due to a key press.

--- common/src/commonMain/kotlin/com/flannaghan/cheetah/common/gui/ListWithKeyboardInteraction.kt
+++ common/src/commonMain/kotlin/com/flannaghan/cheetah/common/gui/ListWithKeyboardInteraction.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.key.*
+import kotlinx.coroutines.launch
 import kotlin.math.max
 
 @Composable
@@ -18,28 +20,34 @@ fun <T> ListWithKeyboardInteraction(
     renderItem: @Composable (index: Int, item: T) -> Unit
 ) {
     val scrollState = rememberLazyListState()
+    val scope = rememberCoroutineScope()
+
+    fun scrollToMakeIndexVisible(index: Int) {
+        // Make sure we stay visible.
+        val startVisible = scrollState.layoutInfo.visibleItemsInfo.firstOrNull()?.index ?: 0
+        val endVisible = scrollState.layoutInfo.visibleItemsInfo.lastOrNull()?.index ?: 0
+        val numberVisible = scrollState.layoutInfo.visibleItemsInfo.size
+        val targetIndex = when {
+            index < startVisible -> index
+            index >= endVisible -> max(index - numberVisible + 2, 0)
+            else -> null
+        }
+        if (targetIndex != null) scope.launch { scrollState.scrollToItem(targetIndex) }
+    }
+
+    // If items changes, scroll to the top.
+    LaunchedEffect(items) { scrollToMakeIndexVisible(0) }
 
     // Scroll, clip indices, etc.
-    fun onChangeSelection_(index: Int) {
+    fun onChangeSelectionViaKeyEvent(index: Int) {
         val clippedIndex = when {
             index < 0 -> 0
             index >= items.size -> items.size - 1
             else -> index
         }
+        scrollToMakeIndexVisible(clippedIndex)
         onChangeSelection(clippedIndex)
     }
-
-    // Make sure we stay visible.
-    val startVisible = scrollState.layoutInfo.visibleItemsInfo.firstOrNull()?.index ?: 0
-    val endVisible = scrollState.layoutInfo.visibleItemsInfo.lastOrNull()?.index ?: 0
-    val numberVisible = scrollState.layoutInfo.visibleItemsInfo.size
-    val targetIndex = when {
-        selectedIndex == null -> null
-        selectedIndex < startVisible -> selectedIndex
-        selectedIndex >= endVisible -> max(selectedIndex - numberVisible + 2, 0)
-        else -> null
-    }
-    if (targetIndex != null) LaunchedEffect(targetIndex) { scrollState.scrollToItem(targetIndex) }
 
     LazyColumn(
         modifier.onPreviewKeyEvent {
@@ -48,19 +56,19 @@ fun <T> ListWithKeyboardInteraction(
                     false
                 }
                 it.key == Key.DirectionUp -> {
-                    if (selectedIndex != null) onChangeSelection_(selectedIndex - 1)
+                    if (selectedIndex != null) onChangeSelectionViaKeyEvent(selectedIndex - 1)
                     true
                 }
                 it.key == Key.DirectionDown -> {
-                    if (selectedIndex != null) onChangeSelection_(selectedIndex + 1)
+                    if (selectedIndex != null) onChangeSelectionViaKeyEvent(selectedIndex + 1)
                     true
                 }
                 it.key == Key.PageUp -> {
-                    onChangeSelection_(0)
+                    onChangeSelectionViaKeyEvent(0)
                     true
                 }
                 it.key == Key.PageDown -> {
-                    onChangeSelection_(items.size - 1)
+                    onChangeSelectionViaKeyEvent(items.size - 1)
                     true
                 }
                 else -> {
