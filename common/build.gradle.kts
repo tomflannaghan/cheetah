de86a38b3b0686d86eaf86f047dfb670cad62035
Tom Flannaghan <tomflannaghan@gmail.com>
2021-05-17 09:14:18 +0100

    Upgrades. Can't yet upgrade to kotlin 1.5.0 here unfortunately due to compose.

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -39,8 +39,8 @@ kotlin {
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test-junit5"))
-                implementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-                runtimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+                implementation("org.junit.jupiter:junit-jupiter-api:5.7.0")
+                runtimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.0")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.3")
             }
         }
@@ -49,7 +49,7 @@ kotlin {
                 api("androidx.appcompat:appcompat:1.3.0-rc01")
                 api("androidx.core:core-ktx:1.3.2")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.4.3")
-                implementation("androidx.compose.runtime:runtime-livedata:1.0.0-beta05")
+                implementation("androidx.compose.runtime:runtime-livedata:1.0.0-beta06")
                 implementation("com.squareup.sqldelight:android-driver:1.5.0")
 
             }
